95de1a6b89b37189b059b91047b0fbab18d7cc44
RyanP <gap41@cam.ac.uk>
2024-02-24 22:31:33 +0000

    Function for 2E

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -9,6 +9,31 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 
 
+def plot_water_levels(station, dates, levels):
+    """
+    Plots the water level data against time for a station,
+    along with the station's typical low and high levels.
+    
+    # Inputs
+    - `station`: the station to plot values for.
+    - `dates`: the dates and times at which the levels were recorded.
+    - `levels`: the water levels recorded at the monitoring station.
+    """
+
+    # Plot
+    plt.plot(t, level)
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45);
+    plt.title(station.name)
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+
+    return plt.show()
+
 def plot_water_level_with_fit(station, dates, levels, p):
     """
     Plots the recent water levels for a station, as well as the fitted polynomial and typical level ranges.
