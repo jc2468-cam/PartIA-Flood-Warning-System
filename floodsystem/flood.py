64ce5d939848da060749dc3057cf51b8c555f963
RyanP <gap41@cam.ac.uk>
2024-02-24 20:32:24 +0000

    2B part 2.2

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -2,6 +2,30 @@
 This module contains functions related to predicting the risk of flooding at monitoring stations.
 """
 
+def stations_level_over_threshold(stations, tol):
+    """
+    Returns a list of tuples, where each tuple holds (i) a station (object) at which the latest relative water level is over tol
+    and (ii) the relative water level at the station.
+    The returned list is sorted by the relative level in descending order.
+
+    # Inputs
+    - `stations`: a `list` of `MonitoringStation`s to sort.
+    - `tol`: the minimum relative water level.
+
+    """
+    over_threshold = list()
+    # Getting a list of station name and latest relative water level over tol
+    for station in stations:
+        a = station.name()
+        b = station.relative_water_level()
+        if b > tol:
+            over_threshold += [(a, b)]
+    
+    # Sorting the list in descending order of relative level
+    return sorted(over_threshold, key = lambda x: x[1], reverse = True)
+    
+               
+
 def stations_highest_rel_level(stations, N):
     """
     Returns a `list` of the first `N` stations with the highest relative water level, sorted by their relative water levels.
