acf5fa10759b93a858c255a204664ccec92c85e3
RyanP <gap41@cam.ac.uk>
2024-02-25 00:10:25 +0000

    2G

---
+++ Task2G.py
@@ -0,0 +1,23 @@
+import datetime
+
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.flood import stations_highest_rel_level
+
+def run():
+    """Requirements for Task 2G"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update water levels
+    update_water_levels(stations)
+
+    
+
+
+
+    
+if __name__ == "__main__":
+    print("*** Task 2F: CUED Part IA Flood Warning System ***")
+    run()
